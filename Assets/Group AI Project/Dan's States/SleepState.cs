e619d5aa51ec73e5927106041f5dac70ab342e67
Donovan Montoya <donovan.montoya1@gmail.com>
2021-09-08 20:48:35 -0600

    Added Sleep State
    Added Sleep State from patrol state.

---
+++ Assets/Group AI Project/Dan's States/SleepState.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SleepState : State {
+    float targetDegree;
+
+    public SleepState(StateController stateController) : base(stateController) { }
+
+    public override void CheckTransitions()
+    {
+        if (stateController.sleepTimer == 2000)
+        {
+            stateController.SetState(new PatrolState(stateController));
+        }
+    }
+    public override void Act()
+    {
+        if (targetDegree < 90)
+        {
+            targetDegree += .1f;
+        }
+
+        Quaternion currentRot = stateController.transform.rotation;
+        Quaternion targetRot = Quaternion.Euler(targetDegree, 0, 0);
+        stateController.transform.rotation = targetRot;
+
+        //if (targetDegree == 90)
+        //{
+        //    targetDegree -= 1f;
+        //    Quaternion currentRot = stateController.transform.rotation;
+        //    Quaternion targetRot = Quaternion.Euler(targetDegree, 0, 0);
+        //    stateController.transform.rotation = targetRot;
+        //}
+
+
+
+    }
+    public override void OnStateEnter()
+    {
+        stateController.sleepTimer = 0;
+        stateController.ChangeColor(Color.yellow);
+        stateController.ai.agent.speed = 0.0f;
+    }
+}
